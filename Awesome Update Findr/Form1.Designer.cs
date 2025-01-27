996d7c06649c3d90133292b4e902d08d8e5b85aa
Giovanni Carlino <97813246+giovannicarlino04@users.noreply.github.com>
2025-01-27 19:05:30 +0100

    Initial commit

---
+++ Awesome Update Findr/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Awesome_Update_Findr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_gameTitle = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.lbl_Url = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 15);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fetch!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(143, 17);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(223, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(1383, 330);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Version";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 82;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Size";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 62;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "SHA1";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 71;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "URL";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.Width = 63;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "FW Version";
+            this.Column5.MinimumWidth = 6;
+            this.Column5.Name = "Column5";
+            this.Column5.Width = 106;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 21);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "TitleID of game:";
+            // 
+            // lbl_gameTitle
+            // 
+            this.lbl_gameTitle.AutoSize = true;
+            this.lbl_gameTitle.Location = new System.Drawing.Point(604, 11);
+            this.lbl_gameTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_gameTitle.Name = "lbl_gameTitle";
+            this.lbl_gameTitle.Size = new System.Drawing.Size(97, 16);
+            this.lbl_gameTitle.TabIndex = 5;
+            this.lbl_gameTitle.Text = "                              ";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_Url);
+            this.splitContainer1.Panel1.Controls.Add(this.lbl_gameTitle);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.textBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridView1);
+            this.splitContainer1.Size = new System.Drawing.Size(1383, 396);
+            this.splitContainer1.SplitterDistance = 61;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // lbl_Url
+            // 
+            this.lbl_Url.AutoSize = true;
+            this.lbl_Url.Location = new System.Drawing.Point(604, 27);
+            this.lbl_Url.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Url.Name = "lbl_Url";
+            this.lbl_Url.Size = new System.Drawing.Size(64, 16);
+            this.lbl_Url.TabIndex = 6;
+            this.lbl_Url.Text = "                   ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(483, 15);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Copy all links";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1383, 396);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Awesome Update Finder 0.1 - NZHAWK";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_gameTitle;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Label lbl_Url;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Awesome Update Findr/Form1.cs
@@ -0,0 +1,137 @@
+﻿/* Awesome Update Finder - NZHawk 2010
+ * 
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ *   I wrote this application at 4am, excuse any errors. Although it does seem to work fine
+ *   it might be a little dodgy :P
+ *   
+ *   oh, icon http://blog.mozilla.com/faaborg/2008/12/16/new-os-x-icons-made-by-sofa/
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.Net;
+using System.Xml.Linq;
+using System.IO;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Awesome_Update_Findr
+{
+    public partial class Form1 : Form
+    {
+        string webUrl;
+        string updateUrl; //L'ho resa pubblica per la funzione che copia i link
+        XmlNodeList elemList;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+                //  Set up the url of the xml file containing the game updates
+                webUrl = "https://a0.ww.np.dl.playstation.net/tpl/np/" + textBox1.Text + "/" + textBox1.Text + "-ver.xml";
+                //  Update status..
+                lbl_Url.Text = "fetching... " + webUrl;
+                //  Needed to allow the certificate
+                ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+                //  Make a new webClient to get the xml file and get/parse it on another thread.
+                WebClient wc = new WebClient();
+                wc.DownloadStringCompleted += HttpsCompleted;
+                wc.DownloadStringAsync(new Uri(webUrl));
+            
+        }
+
+        private void HttpsCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            //  Check if it downloaded the xml file ok
+            if (e.Error == null)
+            {   
+                //  Make an new XmlDocument
+                XmlDocument xdoc = new XmlDocument();
+                // Gotta love try :P
+                try
+                {
+                    //  Load the xml file from e.Result into the XmlDoc
+                    xdoc.LoadXml(e.Result);
+                    //  Make nodeList to hold all the Package Elements
+                    elemList = xdoc.GetElementsByTagName("package");
+                    //  Loop through the list and get each entry
+                    for (int i = 0; i < elemList.Count; i++)
+                    {
+                        //  Obvious?
+                        string updateVersion = elemList[i].Attributes["version"].Value;
+                        string updateSize = elemList[i].Attributes["size"].Value;
+                        string updateSHA1 = elemList[i].Attributes["sha1sum"].Value;
+                        updateUrl = elemList[i].Attributes["url"].Value;
+                        string updateFWVer = elemList[i].Attributes["ps3_system_ver"].Value;
+                        //  Add it to the dataGridView
+                        dataGridView1.Rows.Add(updateVersion, updateSize, updateSHA1, updateUrl, updateFWVer);
+                    }
+                    //  Update the Status labels
+                    lbl_gameTitle.Text = xdoc.SelectSingleNode("/titlepatch/tag/package/paramsfo/TITLE").InnerText;
+                    lbl_Url.Text = webUrl;
+                }
+                //  If error..
+                catch (Exception a)
+                {
+                    //  Show error
+                    showError();
+                }
+            }
+            else // If error..
+            {
+                //  Show error
+                showError();
+            }
+        }
+
+        private void showError()
+        {
+            //  Show error..
+            MessageBox.Show("Error: Title ID not recognised! Please check it and try again.", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            //  Clear stuff
+            lbl_Url.Text = "";
+            lbl_gameTitle.Text = "";
+            dataGridView1.Rows.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string clipboard = "";
+
+            if (elemList != null)
+            {
+                //  Loop through the list and get each entry
+                for (int i = 0; i < elemList.Count; i++)
+                {
+                    updateUrl = elemList[i].Attributes["url"].Value;
+                    clipboard = $"{clipboard} {updateUrl}";
+                }
+                Clipboard.SetText(clipboard);
+            }
+
+
+        }
+    }
+}
